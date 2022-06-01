48ac2fcf568f21fce4043e01efc38f30c33a03a0
Jun Aishima <jaishima@bnl.gov>
2022-06-01 18:52:48 -0400

    task controls the calculation process

--- tasks.py
+++ tasks.py
@@ -1,7 +1,11 @@
 from prefect import task
-from tomo_recon import rotcen_test2
-import databroker
+from load_scan import get_tomo_images
+from tomo_recon_tiled import rotcen_test2
+from tiled.client import from_profile
 
 @task
 def call_find_rot(uid):
-    img, cen = rotcen_test2(uid)
+    c = from_profile('fxi')
+    scan_result = c[uid]
+    img_tomo = get_tomo_images(scan_result) 
+    img, cen = rotcen_test2(img_tomo)
