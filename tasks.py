f41872b8bf29e032c104c57bbf9441cb8a499689
Jun Aishima <jaishima@bnl.gov>
2022-06-02 14:25:07 -0400

    make the task part of a flow

--- tasks.py
+++ tasks.py
@@ -1,4 +1,4 @@
-from prefect import task
+from prefect import task, Flow
 from load_scan import get_tomo_images
 from tomo_recon_tiled import rotcen_test2
 from tiled.client import from_profile
@@ -9,3 +9,8 @@ def call_find_rot(uid):
     scan_result = c[uid]
     img_tomo = get_tomo_images(scan_result) 
     img, cen = rotcen_test2(img_tomo)
+
+with Flow("test-find-rot") as flow1:
+    call_find_rot('123456')
+
+flow1.register(project_name="TST")
