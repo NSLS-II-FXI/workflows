c9b19262045441319b15d36191761fa59bba0e02
Jun Aishima <jaishima@bnl.gov>
2022-07-29 12:06:41 -0400

    remove unused import - flake8

--- load_scan.py
+++ load_scan.py
@@ -2,7 +2,6 @@ from datetime import datetime
 
 import numpy as np
 import pandas as pd
-from databroker.assets.handlers import AreaDetectorHDF5TimestampHandler
 
 EPICS_EPOCH = datetime(1990, 1, 1, 0, 0)
 
