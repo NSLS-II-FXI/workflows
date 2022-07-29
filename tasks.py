4962e7d355c673cc045c858e90226412f78ec7c7
Jun Aishima <jaishima@bnl.gov>
2022-07-29 14:48:04 -0400

    remove unused function

--- tasks.py
+++ tasks.py
@@ -91,58 +91,6 @@ def convert_AD_timestamps(ts):
     )
 
 
-def get_tomo_images(input_dict):
-    pos = input_dict["pos"]
-    imgs = input_dict["imgs"]
-    chunked_timestamps = input_dict["chunked_timestamps"]
-    mot_pos = input_dict["mot_pos"]
-
-    raw_timestamps = []
-    for chunk in chunked_timestamps:
-        raw_timestamps.extend(chunk.tolist())
-
-    timestamps = convert_AD_timestamps(pd.Series(raw_timestamps))
-    pos["time"] = pos["time"].dt.tz_localize("US/Eastern")
-
-    img_day, img_hour = (
-        timestamps.dt.day,
-        timestamps.dt.hour,
-    )
-    img_min, img_sec, img_msec = (
-        timestamps.dt.minute,
-        timestamps.dt.second,
-        timestamps.dt.microsecond,
-    )
-    img_time = (
-        img_day * 86400 + img_hour * 3600 + img_min * 60 + img_sec + img_msec * 1e-6
-    )
-    img_time = np.array(img_time)
-
-    mot_day, mot_hour = (
-        pos["time"].dt.day,
-        pos["time"].dt.hour,
-    )
-    mot_min, mot_sec, mot_msec = (
-        pos["time"].dt.minute,
-        pos["time"].dt.second,
-        pos["time"].dt.microsecond,
-    )
-    mot_time = (
-        mot_day * 86400 + mot_hour * 3600 + mot_min * 60 + mot_sec + mot_msec * 1e-6
-    )
-    mot_time = np.array(mot_time)
-
-    offset = np.min([np.min(img_time), np.min(mot_time)])
-    img_time -= offset
-    mot_time -= offset
-    mot_pos_interp = np.interp(img_time, mot_time, mot_pos)
-
-    pos2 = mot_pos_interp.argmax() + 1
-    img_angle = mot_pos_interp[: pos2 - chunk_size]  # rotation angles
-    img_tomo = imgs[: pos2 - chunk_size]  # tomo images
-    return img_tomo, img_angle
-
-
 def find_nearest(data, value):
     data = np.array(data)
     return np.abs(data - value).argmin()
