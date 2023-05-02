071c69a19371f3fc13a03425dd9a19fcd2748d83
Abigail Giles <agiles@bnl.gov>
2023-05-02 15:11:16 -0400

    Update end of run workflow to prefect 2

--- end_of_run_workflow.py
+++ end_of_run_workflow.py
@@ -1,26 +1,17 @@
-import prefect
-from prefect import task, Flow, Parameter
-from prefect.tasks.prefect import create_flow_run
+from prefect import task, flow, get_run_logger
+from data_validation import general_data_validation
+# TODO: export needs Prefect 2 migration
+# from export import export
 
 
 @task
 def log_completion():
-    logger = prefect.context.get("logger")
+    logger = get_run_logger()
     logger.info("Complete")
 
-
-with Flow("end-of-run-workflow") as flow:
-    stop_doc = Parameter("stop_doc")
+@flow
+def end_of_run_workflow(stop_doc):
     uid = stop_doc["run_start"]
-    validation_flow = create_flow_run(
-        flow_name="general-data-validation",
-        project_name="FXI",
-        parameters={"beamline_acronym": "fxi" ,"uid": uid}
-    )
-    export_flow = create_flow_run(
-        flow_name="export",
-        project_name="FXI",
-        parameters={"uid": uid}
-    )
-    log_completion(upstream_tasks=[validation_flow, export_flow])
-
+    general_data_validation(uid)
+    # export(uid)
+    log_completion()
